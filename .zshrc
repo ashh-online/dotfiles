4f29e922ce3cafbf8f4e67b24691d51f5814f0de
Ashley Hauck <ashley@bytespell.com>
2026-01-18 09:14:30 -0600

    Add autosuggestions for zsh and history search for bash

--- .zshrc
+++ .zshrc
@@ -8,3 +8,8 @@ setopt HIST_IGNORE_DUPS
 
 # Custom prompt
 PROMPT='%F{magenta}ashh%f %% '
+
+# Autosuggestions (grayed-out history completion)
+[ -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -f /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
--- setup.sh
+++ setup.sh
@@ -2,13 +2,30 @@
 
 DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 
+# Detect OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    INSTALL_CMD="brew install"
+elif command -v apt &> /dev/null; then
+    INSTALL_CMD="sudo apt install -y"
+else
+    INSTALL_CMD=""
+fi
+
 if [ -n "$ZSH_VERSION" ] || [ "$SHELL" = "/bin/zsh" ]; then
     echo "Setting up zsh..."
     ln -sf "$DOTFILES/.zshrc" ~/.zshrc
+
+    # Install autosuggestions
+    if [ -n "$INSTALL_CMD" ]; then
+        echo "Installing zsh-autosuggestions..."
+        $INSTALL_CMD zsh-autosuggestions
+    fi
+
     echo "Done. Run: source ~/.zshrc"
 elif [ -n "$BASH_VERSION" ] || [ "$SHELL" = "/bin/bash" ]; then
     echo "Setting up bash..."
     ln -sf "$DOTFILES/.bashrc" ~/.bashrc
+    ln -sf "$DOTFILES/.inputrc" ~/.inputrc
     echo "Done. Run: source ~/.bashrc"
 else
     echo "Unknown shell: $SHELL"
